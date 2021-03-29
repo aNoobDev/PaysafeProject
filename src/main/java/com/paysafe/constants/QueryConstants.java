8f8c14971b120e13d74db4d28f233c25b55284e7
aNoobDev <alamaftab777@gmail.com>
2021-03-29 16:19:36 +0530

    new db

--- src/main/java/com/paysafe/constants/QueryConstants.java
+++ src/main/java/com/paysafe/constants/QueryConstants.java
@@ -2,7 +2,7 @@ package com.paysafe.constants;
 
 public interface QueryConstants {
 
-	String CUSTOMER_CHECK = "SELECT merchantCustId,id, paymentToken  FROM customer_detail WHERE emailId = ? and phone = ?";
-	String CREATE_CUSTOMER = "INSERT INTO customer_detail (id, paymentToken, firstName, emailId, phone, merchantCustId) VALUES(?,?,?,?,?,?)";
+	String CUSTOMER_CHECK = "SELECT merchantCustId,id, paymentToken  FROM paysafe_customer WHERE emailId = ? and phone = ?";
+	String CREATE_CUSTOMER = "INSERT INTO paysafe_customer (id, paymentToken, firstName, emailId, phone, merchantCustId) VALUES(?,?,?,?,?,?)";
 
 }
